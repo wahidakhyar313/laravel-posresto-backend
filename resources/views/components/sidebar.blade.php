145043688d8a30d5984615fbeebfc603761946ff
akhyar313 <akhyar313wachid@gmail.com>
2024-02-10 19:26:48 +0700

    init resto backend

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,35 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">RESTO WAHID</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="nav-item dropdown">
+                <a href="#"
+                    class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <ul class="dropdown-menu">
+                    <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
+                        <a class="nav-link"
+                            href="{{ url('dashboard-general-dashboard') }}">General Dashboard</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="menu-header">Menu</li>
+            <li class="nav-item dropdown">
+                <a href="{{ route('users.index') }}"
+                    class="nav-link"><i class="fas fa-user"></i><span>Users</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="{{ route('users.index') }}"
+                    class="nav-link"><i class="fas fa-box"></i><span>Products</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="{{ route('users.index') }}"
+                    class="nav-link"><i class="fas fa-boxes-stacked"></i><span>Categories</span></a>
+            </li>
+    </aside>
+</div>
